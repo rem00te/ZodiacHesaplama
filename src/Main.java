4a33df2fc30d042413f050457724f06ac1a15693
Onur ALTINTOP <rem00te@hotmail.com>
2022-07-12 11:19:12 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int yil;
+
+        System.out.println(" Lütfen doğum yılınızı giriniz : ");
+        yil = input.nextInt();
+
+        switch (yil % 12) {
+            case 0:
+                System.out.println("Maymun Burcu");
+                break;
+            case 1:
+                System.out.println("Horoz Burcu");
+                break;
+            case 2:
+                System.out.println("Köpek Burcu ");
+                break;
+            case 3:
+                System.out.println("Domuz Burcu ");
+                break;
+            case 4:
+                System.out.println("Fare Burcu ");
+                break;
+            case 5:
+                System.out.println("Öküz Burcu");
+                break;
+            case 6:
+                System.out.println("Kaplan Burcu");
+                break;
+            case 7:
+                System.out.println("Tavşan Burcu");
+                break;
+            case 8:
+                System.out.println("Ejderha Burcu ");
+                break;
+            case 9:
+                System.out.println("Yılan Burcu ");
+                break;
+            case 10 :
+                System.out.println("At Burcu");
+                break;
+            case 11:
+                System.out.println("Koyun Burcu ");
+                break;
+        }
+    }
+}
